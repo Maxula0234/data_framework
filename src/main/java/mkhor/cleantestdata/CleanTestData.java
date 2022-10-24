d0ac29459a7ab5912f0b06fa47b345657fd05734
max <max0234@ixloud.com>
2022-10-24 14:34:05 +0400

    ipdate

--- src/main/java/mkhor/cleantestdata/CleanTestData.java
+++ src/main/java/mkhor/cleantestdata/CleanTestData.java
@@ -2,8 +2,10 @@ package mkhor.cleantestdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class CleanTestData {
 
 	public static void main(String[] args) {
--- src/test/java/mkhor/cleantestdata/AccountTest.java
+++ src/test/java/mkhor/cleantestdata/AccountTest.java
@@ -5,7 +5,6 @@ import mkhor.cleantestdata.api.service.accounts.AccountsService;
 import mkhor.cleantestdata.utils.DateUtils;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -16,7 +15,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,45 +30,25 @@ public class AccountTest extends BaseTest {
     public AccountsService accountsService;
 
     public static Stream<Arguments> getAcc() {
-        return Stream.of(
-                Arguments.of("id"),
-                Arguments.of("accountNumber")
-        );
+        return Stream.of(Arguments.of("id"), Arguments.of("accountNumber"));
     }
 
     public Account addAccountPrecondition() {
         var amount = new BigDecimal(String.valueOf(faker.random().nextInt(1111, 9999)));
         var owner = new StringBuilder();
-        owner.append(faker.name().firstName())
-                .append(" ")
-                .append(faker.name().lastName());
+        owner.append(faker.name().firstName()).append(" ").append(faker.name().lastName());
 
         String product = "МИР";
         String date_create = DateUtils.parseDateToString(faker.date().birthday());
         String number = "40702810" + faker.random().nextInt(1111, 9999) + "60657001";
         long owner_id = faker.number().randomNumber();
 
-        var account = Account.builder()
-                .account_product(product)
-                .amount(amount)
-                .date_create(date_create)
-                .number(number)
-                .owner(owner.toString())
-                .owner_id(owner_id)
-                .reserved(false)
-                .build();
+        var account = Account.builder().account_product(product).amount(amount).date_create(date_create).number(number).owner(owner.toString()).owner_id(owner_id).reserved(false).build();
 
         Account acc = accountsService.addAccount(account);
 
         Account newAccount = accountsService.getAccount(acc.getID());
-        assertAll(
-                () -> assertThat(newAccount.getAccount_product()).isEqualToIgnoringCase(product),
-                () -> assertThat(newAccount.getNumber()).isEqualToIgnoringCase(number),
-                () -> assertThat(newAccount.getAmount()).isEqualTo(amount),
-                () -> assertThat(newAccount.getOwner()).isEqualToIgnoringCase(owner),
-                () -> assertThat(newAccount.getOwner_id()).isEqualTo(owner_id),
-                () -> assertThat(newAccount.getDate_create()).isEqualTo(date_create)
-        );
+        assertAll(() -> assertThat(newAccount.getAccount_product()).isEqualToIgnoringCase(product), () -> assertThat(newAccount.getNumber()).isEqualToIgnoringCase(number), () -> assertThat(newAccount.getAmount()).isEqualTo(amount), () -> assertThat(newAccount.getOwner()).isEqualToIgnoringCase(owner), () -> assertThat(newAccount.getOwner_id()).isEqualTo(owner_id), () -> assertThat(newAccount.getDate_create()).isEqualTo(date_create));
 
         return newAccount;
     }
@@ -83,16 +64,8 @@ public class AccountTest extends BaseTest {
     void getAllAccounts() {
         List<Account> allAccounts = accountsService.getAllAccounts();
         assertThat(allAccounts).allSatisfy(acc -> {
-                    assertAll(
-                            () -> assertThat(acc.getAccount_product()).isNotNull(),
-                            () -> assertThat(acc.getNumber()).isNotNull(),
-                            () -> assertThat(acc.getAmount()).isNotNull(),
-                            () -> assertThat(acc.getOwner()).isNotNull(),
-                            () -> assertThat(acc.getOwner_id()).isNotNull(),
-                            () -> assertThat(acc.getDate_create()).isNotNull()
-                    );
-                }
-        );
+            assertAll(() -> assertThat(acc.getAccount_product()).isNotNull(), () -> assertThat(acc.getNumber()).isNotNull(), () -> assertThat(acc.getAmount()).isNotNull(), () -> assertThat(acc.getOwner()).isNotNull(), () -> assertThat(acc.getOwner_id()).isNotNull(), () -> assertThat(acc.getDate_create()).isNotNull());
+        });
     }
 
     @ParameterizedTest
@@ -116,14 +89,7 @@ public class AccountTest extends BaseTest {
         }
 
         var accFinal = account;
-        assertAll(
-                () -> assertThat(accFinal.getAccount_product()).isNotNull(),
-                () -> assertThat(accFinal.getNumber()).isNotNull(),
-                () -> assertThat(accFinal.getAmount()).isNotNull(),
-                () -> assertThat(accFinal.getOwner()).isNotNull(),
-                () -> assertThat(accFinal.getOwner_id()).isNotNull(),
-                () -> assertThat(accFinal.getDate_create()).isNotNull()
-        );
+        assertAll(() -> assertThat(accFinal.getAccount_product()).isNotNull(), () -> assertThat(accFinal.getNumber()).isNotNull(), () -> assertThat(accFinal.getAmount()).isNotNull(), () -> assertThat(accFinal.getOwner()).isNotNull(), () -> assertThat(accFinal.getOwner_id()).isNotNull(), () -> assertThat(accFinal.getDate_create()).isNotNull());
     }
 
     @Test
@@ -133,9 +99,7 @@ public class AccountTest extends BaseTest {
         accountsService.deleteAccount(account.getID());
         List<Account> allAccounts = accountsService.getAllAccounts();
 
-        Optional<Account> finAcc = allAccounts.stream()
-                .filter(acc -> acc.getID() == account.getID())
-                .findFirst();
+        Optional<Account> finAcc = allAccounts.stream().filter(acc -> acc.getID() == account.getID()).findFirst();
 
         assertThat(finAcc).as("Счет не удален").isNotPresent();
     }
@@ -146,12 +110,34 @@ public class AccountTest extends BaseTest {
         List<Account> allAccounts = accountsService.getAllAccounts();
         Account account = allAccounts.get(0);
 
-        account.setOwner(faker.name().firstName() + " " +
-                faker.name().lastName());
+        account.setOwner(faker.name().firstName() + " " + faker.name().lastName());
 
         accountsService.updateAccount(account.getID(), account);
         Account newAccount = accountsService.getAccount(account.getID());
 
         assertThat(account).isEqualTo(newAccount);
+    }
+
+    @Test
+    public void teststts() {
+        /**
+         * Есть коллеция с объявлениями
+         * создать функцию, которая отфильтрует мапу и выдаст только те цены которые указанны пользователем
+         *
+         */
+
+        Map<String, Integer> ds = Map.of("Petr", 100, "Max", 123, "ivan", 123123, "sergey", 43343, "Stepan", 554, "Enot", 553);
+
+
+        filter(ds, 200);
+        filter(ds, 600);
+
+
+    }
+
+    private void filter(Map<String, Integer> ds, int amount) {
+        Map<String, Integer> collect = ds.entrySet().stream().filter(item -> item.getValue() > amount).collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
+
+        System.out.println(collect);
     }
 }
